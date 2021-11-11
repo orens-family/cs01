5dac7458b545728204e35c5bb06281cfd49ed604
Steve Orens <sorens@users.noreply.github.com>
2021-11-11 03:50:51 +0000

    Lesson 01 Variables

---
+++ L01/hello.py
@@ -0,0 +1,4 @@
+daddy="whyyyyyyyyyyyyyyyyyy my arm hurts ahhh why does it half to hurt ugggg, i wonder if harry potter ever got a vaxination , i wonder if his arm hurt from it"
+print(daddy)
+Daddy="hiiiii"
+print(Daddy)
